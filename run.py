954f45561d006f16513ae3f670412ea89fe4a497
carlr <lissettecarlr@163.com>
2021-12-19 10:31:22 +0800

    fix space

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ keyDict={
 'Lmenu':'altleft', 'Rmenu':'altright', 'Rcontrol':'ctrlright', 'Escape':'esc', 'F1':'f1', 'F2':'f2', 'F3':'f3', 'F4':'f4', 'F5':'f5',
 'F6':'f6', 'F7':'f7', 'F8':'f8', 'F9':'f9', 'F10':'f10', 'F11':'f11', 'F12':'f12', 'Home':'home', 'End':'end', 'Insert':'insert',
 'Delete':'delete',
-'Lwin':'winleft','Rwin':'winright'
+'Lwin':'winleft','Rwin':'winright','Space':'space'
 }
 
 class Runner(threading.Thread):
